15939744e7a1972096df4a5f2a2549843243485e
ildacornolius <ildacorn@gmail.com>
2022-09-30 17:29:15 -0400

    Added new files2

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ router.use(
 
 app.use(router);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
 
