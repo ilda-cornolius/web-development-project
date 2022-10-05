6e58cb8c6f0137adf27d41a73e17744789406936
ildacornolius <ildacorn@gmail.com>
2022-10-04 21:39:22 -0400

    Added new files222

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Index 
+// Ildaphonse Cornolius
+// ID: 300699371
+// Date: October 2nd, 2022
+
+//the index javascript
+//loading the express / path and other objects to use 
 const { render } = require("ejs");
 const express = require("express");
 const path = require("path")
@@ -5,104 +12,36 @@ const router = require('./routes/router');
 // const router = express.Router()
 
 
+//creating an express instance
 const app = express();
+//using the router package in our app
 app.use(router);
 
 app.use(express.static("public"));
 
 
 app.set('view engine', 'ejs');
+//using port 3000
 const port = 3000;
 
 
 
-
+//setting the path for our css files 
 router.use(
     "/css",
     express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css"))
   )
+
+  //setting the path for our javascript files 
   router.use(
     "/js",
     express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js"))
   )
   router.use("/js", express.static(path.join(__dirname, "../node_modules/jquery/dist")))
   
-// router.get('/', function(req, res, next){
-
-//     res.render("index",{title:"Assignment 1"})
-// })
-
-// // app.get("/", (req, res) => {
-// // //   res.send("Hello, World!");
-
-// //   res.render("index",{title:"Assignment 1"})
-
-
-// // });
-
-
-// router.get("/index", (req, res) => {
-//     //   res.send("Hello, World!");
-//     var query=req.query;
-
-//     console.log("QUERY:");
-//     console.log(req.query);
-//       //res.render("index",{title:"Assignment 1"})
-//       if(query){
-//         var s = "";
-//         for(var p in query){
-//             s+=p+" : "+query[p]+" <br>";
-//         }
-//         //res.render("index",{title: JSON.stringify(query)})
-//         res.render("index",{title: s})
-
-//       }
-//       else{
-//       res.render("index",{title:"Assignment 1"})
-//     }
-    
-//     });
-
-
-
-
-// router.get("/home", (req, res) => {
-
-//     res.render("index");
-
-//   });
-
-//   router.get("/about", (req, res) => {
-//     res.render("about");
-
-//   });
-
-//   router.get("/projects", (req, res) => {
-//     res.render("projects");
-//   });
-
-//   router.get("/services", (req, res) => {
-//     res.render("services");
-//   });
-
-// router.get("/contact", (req, res) => {
-//   res.render("contact");
-// });
-
-// router.get("/index.html", function (req, res) {
-//   res.sendFile(__dirname + "/" + "index.html");
-// });
-// router.get("/process_get", function (req, res) {
-//   response = {
-//     first_name: req.query.first_name,
-//     last_name: req.query.last_name,
-//   };
-//   console.log(response);
-//   res.end(JSON.stringify(response));
-// });
-
 app.use(router);
 
+//listening for the webpage on port 3000, or a custom port set by a cloud service 
 app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 })
